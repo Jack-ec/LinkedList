d5d67ae659195f685f3131473e3632a5c69e899a
jchid <jchid@LAPTOP-LV3MAVIE>
2021-12-01 22:22:37 -0700

    i do not understand whats happening

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,4 +1,47 @@
 
 public class LinkedList {
+	Node head = null;
+	Node tail = null;
+	public void add(String element) {
+		Node newNode = new Node(element, null, null);  
+        if(head == null) {   
+            head = tail = newNode;  
+            head.previous = null;  
+            tail.next = null;  
+        }  
+        else {  
+            tail.next = newNode;  
+            newNode.previous = tail;  
+            tail = newNode;  
+            tail.next = null;  
+        }  
+	}
+
+	public void add(int index, String element) {
+		
+	}
+
+	public String toString() {
+		
+	}
+
+	public int size() {
+	}
+
+	public void clear() {
+		
+	}
+
+	public String get(int index) {
+		
+	}
+
+	public void set(int index, String element) {
+		
+	}
+
+	public void remove(int index) {
+		
+	}
 
 }
