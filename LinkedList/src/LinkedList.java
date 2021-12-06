bb935e35208ed8656a077d47cb75bbdcd300a507
904154001 <904154001@L829WD150074.edu.cbe.ab.ca>
2021-12-06 09:47:20 -0700

    no clue what im doing

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -5,18 +5,18 @@ public class LinkedList {
 	int size = 0;
 	public void add(String element) {
 		Node newNode = new Node(element, null, null);  
-        if(head == null) {   
-            head = tail = newNode;  
-            head.previous = null;  
-            tail.next = null;  
-        }  
-        else {  
-            tail.next = newNode;  
-            newNode.previous = tail;  
-            tail = newNode;  
-            tail.next = null;  
-        }  
-        size++;
+		if(head == null) {   
+			head = tail = newNode;  
+			head.previous = null;  
+			tail.next = null;  
+		}  
+		else {  
+			tail.next = newNode;  
+			newNode.previous = tail;  
+			tail = newNode;  
+			tail.next = null;  
+		}  
+		size++;
 	}
 
 	public void add(int index, String element) {
@@ -35,52 +35,68 @@ public class LinkedList {
 	}
 
 	public String toString() {
-		
-	}
+		Node current = head;  
+		if(head == null) {  
+			return null;  
+		}  
+		while(current != null) {  
+			
+		}
+	}  
+
 
 	public int size() {
 		return size;
-		
+
 	}
 
 	public String get(int index) {
 		if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        } else {
-            Node current = head;
-            for (int i = 0; i < index; i++) {
-                current = current.next;
-            }
-            return current.value;
-        }
-    }
+			throw new IndexOutOfBoundsException();
+		} else {
+			Node current = head;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			return current.value;
+		}
+	}
 	public void set(int index, String element) {
-		
+
 	}
 
 	public void remove(int index) {
-	    if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        } else if (index == 0) {
-            head = head.next;
-            head.previous = null;
-            size--;
-        } else if (index == size - 1) {
-            tail = tail.previous;
-            tail.next = null;
-            size--;
-        } else {
-            Node current = head;
-            for (int i = 0; i < index; i++) {
-                current = current.next;
-            }
-            Node previous = current.previous;
-            Node next = current.next;
-            previous.next = current.next;
-            next.previous = previous;
-            size--;
-        }
-    }
-
-		
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		} else if (index == 0) {
+			head = head.next;
+			head.previous = null;
+			size--;
+		} else if (index == size - 1) {
+			tail = tail.previous;
+			tail.next = null;
+			size--;
+		} else {
+			Node current = head;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			Node previous = current.previous;
+			Node next = current.next;
+			previous.next = current.next;
+			next.previous = previous;
+			size--;
+		}
 	}
+	public void clear() {
+		 Node temp = new Node(null, null, null);
+		  while(this.head != null) {
+		    temp = this.head;
+		    this.head = this.head.next;
+		    temp = null;
+		  }
+
+	}
+
+
+}
